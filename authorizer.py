f484311f04fd0db8a750a7b8ce4604fc40494aeb
Rui Constancio <rui.constancio@protonmail.com>
2021-06-27 22:55:52 -0300

    Refactor to external handlers

--- authorizer.py
+++ authorizer.py
@@ -1,10 +1,8 @@
 import json
 from sys import stdin
 
-from core.account import Account
-from core.handlers import handle_transaction
+from core.handlers import handle_account, handle_transaction
 
-IS_ACCOUNT_SET=False
 
 if __name__ == "__main__":
     acc = None
@@ -14,17 +12,8 @@ if __name__ == "__main__":
         transaction_request = json.loads(line)
         violation = None
         if 'account' in transaction_request.keys():
-            if not IS_ACCOUNT_SET:
-                acc = Account(
-                    available_limit=transaction_request['account']['availableLimit'],
-                    active_card=transaction_request['account']['activeCard']
-                )
-                response = json.dumps({'account': {'activeCard': acc.active_card, 'availableLimit': acc.available_limit}, 'violations': []})
-                output.append(response)
-                IS_ACCOUNT_SET=True
-            else:
-                response = json.dumps({'account': {'activeCard': acc.active_card, 'availableLimit': acc.available_limit}, 'violations': ['account-already-initialized']})
-                output.append(response)
+            acc, response = handle_account(account_request=transaction_request, account=acc)
+            output.append(response)
         elif 'transaction' in transaction_request.keys():
             response = handle_transaction(account=acc, transaction_request=transaction_request)
             output.append(response)
